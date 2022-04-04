767fbf0d8d1e62236c145621dadff2920c758cc7
리펙스 <66472285+xoals35@users.noreply.github.com>
2022-04-04 21:59:01 +0900

    Update 패치노트.js

--- commands/패치노트.js
+++ commands/패치노트.js
@@ -8,13 +8,18 @@ module.exports = {
   async execute(client,message,args) {
     const embed1 = new MessageEmbed()
     .setColor("YELLOW")
-    .setTitle("패치노트 1.01")
+    .setTitle("패치노트 1.01(3월20일)")
     .addField("추가된 명령어", "경고, 뮤트, 언뮤트, 밴 등이 추가되었습니다")
     
     const embed2 = new MessageEmbed()
     .setColor("GREEN")
-    .setTitle("패치노트 1.02")
+    .setTitle("패치노트 1.02(4월4일)")
     .addField("경고 개선", '경고 기능이 개선 되었습니다. t.경고 @유저맨션 @경고1회(아니면 2회)역할 맨션 하시면 됩니다.')
+    
+     const embed3 = new MessageEmbed()
+    .setColor("GREEN")
+    .setTitle("패치노트 1.03(4월4일)")
+    .addField("뮤직 명령어 추가", '현재 추가된 명령어:t.재생, t.현재재생, t.스킵, t.')
     
     const emojiList = ['⏪', '⏩']
 
